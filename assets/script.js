7055e13ed998ae4147d045584c8dffe65bfc8043
Matt Lemke <mattrlemke@gmail.com>
2021-03-13 15:07:54 -0600

    cracked into the id

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,34 @@
-
-
 $(document).ready(function(){
 
 
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"))
     let children = $("tbody").children()
+
 for (let i = 0; i < children.length; i++)  {
 
+
     var childTr = children.eq(i)
-    var time = childTr.id
+    
+    var time = childTr[0].id
+   console.log(time)
     var trChildren = childTr.children();
+    var key = trChildren.id
+
+
     var textArea = trChildren.eq(1);
     var button = trChildren.eq(2).find("button")
+   
     
     if (localStorage.getItem(time) === null) {
     var Items = localStorage.getItem(time)
     textArea.innerHTML = Items
+   
 
       }
 
     button.on("click", function(Save) {
 
-    localStorage.setItem(time, Items)
+    localStorage.setItem(time, textArea)
     
 
     })
