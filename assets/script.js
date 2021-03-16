ff4ce6b027f4d143a9311b10214f3907d5f8e8ef
Matt Lemke <mattrlemke@gmail.com>
2021-03-15 21:31:04 -0500

    finallygit add .

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,52 @@
-var timeKey, value, valueText, block
-
-
-
 $(document).ready(function(){
   
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"))
   let children = $("tbody").children()
 
+$("tbody").find("tr").each(function() { 
+ document.getElementById("8-text").innerHTML = (localStorage.getItem("8"));
+ document.getElementById("9-text").innerHTML = (localStorage.getItem("9"));
+ document.getElementById("10-text").innerHTML = (localStorage.getItem("10"));
+ document.getElementById("11-text").innerHTML = (localStorage.getItem("11"));
+ document.getElementById("12-text").innerHTML = (localStorage.getItem("12"));
+ document.getElementById("13-text").innerHTML = (localStorage.getItem("13"));
+ document.getElementById("14-text").innerHTML = (localStorage.getItem("14"));
+ document.getElementById("15-text").innerHTML = (localStorage.getItem("15"));
+ document.getElementById("16-text").innerHTML = (localStorage.getItem("16"));
+ document.getElementById("17-text").innerHTML = (localStorage.getItem("17"));
 
-
+ 
   })
 
-  $("button").on("click", function(){
-
-   var timeKey = $(this).parent().attr("id")
+  $("button").on("click", function(timeKey, valueText){
+    var timeKey = $(this).parent().attr("id");
     // console.log(timeKey)
-  
-    value = $(this).closest("tr");
-    valueText = value.find("td:eq(0)").text();
+    var value = $(this).closest("tr");
+    var valueText = value.find("td:eq(0)").text();
     // console.log(valueText)
 
-    block = localStorage.setItem(timeKey, valueText)
+
+   localStorage.setItem(timeKey, valueText)
     
 
   })
-  $("#9 .toDo").val(localStorage.getItem("9"))
+})
 
-//   for (let i = 0; i < localStorage.length; i++) {
-//     var set = localStorage.key(i);
-//     console.log(set)
-//     if (set === timeKey) {
-//     JSON.parse(localStorage.getItem(timeKey))
-//     }
-// }
+// set color blocks
 
+let TimeOfDay = function () {
+  let currentTime = moment().hours()
 
-  
+$(".tasks").each(function(){
+  let pastTime = parseInt($(this).attr("id"))
+  console.log(pastTime)
+  if(pastTime < currentTime) {
+    $(this).addClass("table-dark")
+  }
+})
 
- 
- 
+    }
+
   
    
    
@@ -59,50 +67,5 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-
-//   for (let i = 0; i < children.length; i++)  {
-//     // grab id to put in local storage
-//     
-    
-    
-
-//     // grab text to put in local storage
-//     var trChildren = childTr.children();
-//     // console.log(trChildren)
-//     var textArea = trChildren.eq(1)
-//     // console.log(textArea)
-    
-//     var button = trChildren.eq(2).find("button")
-
-//     button.on("click", function() {
-//       //this code below will become your "block" variable
-//       
-//       //this set msg to localstorage
-//    
-      
-//    
-//     // console.log(Items)
-//     }
-
-//   }
-
-
-/*
-function that loads items into saved positions
-function that saves items
-save button onclick handler function
-function that audits times and styles dynamically
-
-.val() gives value of input field
-.text() gives text within eleem
-*/
-
-// let glenVar = $("#textarea").val();
-// console.log(glenVar)
 
     
